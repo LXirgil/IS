3a79f037fae5811cd06649f9ec9e3f230efc1f14
LXirgil <hikkyhachiman0920@gmail.com>
2026-06-03 15:30:47 +0900

    Lazy-init AuthService to avoid FirebaseAuth access before app init

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,24 +2,27 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   AuthService._private();
-  static final AuthService instance = AuthService._private();
+  static AuthService? _instance;
+  static AuthService get instance => _instance ??= AuthService._private();
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseAuth? _auth;
 
-  Stream<User?> get authStateChanges => _auth.authStateChanges();
+  FirebaseAuth get _firebaseAuth => _auth ??= FirebaseAuth.instance;
 
-  User? get currentUser => _auth.currentUser;
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  User? get currentUser => _auth?.currentUser;
 
   Future<UserCredential> signInWithEmail(String email, String password) async {
-    return await _auth.signInWithEmailAndPassword(email: email, password: password);
+    return await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
   }
 
   Future<UserCredential> registerWithEmail(String email, String password) async {
-    return await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    return await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
   }
 
   Future<void> signOut() async {
-    await _auth.signOut();
+    await _firebaseAuth.signOut();
   }
 }
 
